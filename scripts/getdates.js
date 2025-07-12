f854c90a2c6b795127d6026ed4915272b13b3717
David Labra Gaona <davidlabragaona@gmail.com>
2025-07-12 14:40:42 -0300

    w01 home page submit

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const year = document.querySelector("#currentyear");
+const lastUpdate = document.querySelector("#lastModified");
+
+year.textContent = today.getFullYear();
+lastUpdate.textContent = `Last modification: ${document.lastModified}`;
+
