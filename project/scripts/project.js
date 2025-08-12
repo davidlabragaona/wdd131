a14f0f0a396307fcc3f4eaf7c92753a508088e05
David Labra Gaona <davidlabragaona@gmail.com>
2025-08-12 15:52:23 -0300

    w05 website planning document

---
+++ project/scripts/project.js
@@ -0,0 +1,14 @@
+const today = new Date();
+const year = document.querySelector("#currentyear");
+const lastUpdate = document.querySelector("#lastModified");
+
+year.textContent = today.getFullYear();
+lastUpdate.textContent = `Last modification: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	menuButton.classList.toggle("open");
+});
