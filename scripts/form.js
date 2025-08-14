71eef447d05de7c098ab69566399ae31b551a099
David Labra Gaona <davidlabragaona@gmail.com>
2025-08-14 10:45:46 -0300

    fix

--- scripts/form.js
+++ scripts/form.js
@@ -40,11 +40,3 @@ products.forEach(x => {
 
 year.textContent = today.getFullYear();
 lastUpdate.textContent = `Last modification: ${document.lastModified}`;
-
-const menuButton = document.querySelector("#menu");
-const navigation = document.querySelector("nav");
-
-menuButton.addEventListener("click", () => {
-	navigation.classList.toggle("open");
-	menuButton.classList.toggle("open");
-});
