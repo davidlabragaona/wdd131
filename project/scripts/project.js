32120659e3494972a0e99a44b8626ce468d3728c
David Labra Gaona <davidlabragaona@gmail.com>
2025-08-14 16:48:09 -0300

    almost done

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,14 +1,69 @@
 const today = new Date();
 const year = document.querySelector("#currentyear");
 const lastUpdate = document.querySelector("#lastModified");
-
 year.textContent = today.getFullYear();
 lastUpdate.textContent = `Last modification: ${document.lastModified}`;
 
-const menuButton = document.querySelector("#menu");
+/*const menuButton = document.querySelector("#menu");
 const navigation = document.querySelector("nav");
 
 menuButton.addEventListener("click", () => {
 	navigation.classList.toggle("open");
 	menuButton.classList.toggle("open");
 });
+*/
+/* -------  If we created a fishtank  ----------*/
+checkForm();
+loadData();
+
+function checkForm() {
+	let params = new URLSearchParams(window.location.search);
+	if (params.size === 0)
+		return;
+	let fishData = {};
+
+	for (let [key, value] of params) {
+		//console.log(`${key} = ${value}`);
+		fishData[key] = value;
+	}
+	localStorage.setItem('fishdata', JSON.stringify(fishData));
+}
+
+/*---- Global ----*/
+function getData(data) {
+	return localStorage.getItem(data) || null;
+}
+
+function loadData() {
+	let tankdata = getData('tankdata');
+	let fishdata = getData('fishdata');
+	if (tankdata !== null)
+		tankdata = JSON.parse(tankdata);
+	displayTank(tankdata);
+	if (fishdata !== null)
+		fishdata = JSON.parse(fishdata);
+	displayfish(fishdata);
+}
+
+function displayTank(tankdata) {
+	if (tankdata === null)
+	{
+		let main = document.querySelector('main');
+		main.innerHTML = '<section class="card"><h3>No tank defined. Please create one</h3></section>';
+	}
+	else
+	{
+		
+	}
+}
+
+function displayFish(fishdata) {
+	if (fishdata === null) {
+
+	}
+	else {
+
+	}
+	
+}
+
---
+++ project/scripts/shop.js
@@ -0,0 +1,26 @@
+const purchase = document.querySelector('#fish-purchase');
+
+purchase.addEventListener('click', (e) => {
+    if (e.target.tagName === 'BUTTON') {
+        saveData(e.target['id']);
+    }
+});
+
+function saveData(fish) {
+    let data = getFishData();
+    if (fish === 'gold')
+        data['gold']++;
+    else if (fish === 'clown')
+        data['clown']++;
+    else
+        data['cardinal']++;
+    localStorage.setItem('fish', JSON.stringify(data));
+}
+
+function getFishData() {
+    let data = localStorage.getItem('fish');
+    if (data === null)
+        return {gold: 0, clown: 0, cardinal: 0};
+    else
+        return JSON.parse(data);
+}
