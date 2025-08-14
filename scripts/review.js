7660619c054b32f20e2d4abef0ac06aa04ff79a4
David Labra Gaona <davidlabragaona@gmail.com>
2025-08-14 11:12:14 -0300

    final form

--- scripts/review.js
+++ scripts/review.js
@@ -1,8 +1,14 @@
+const visits = document.querySelector("#visits");
+
 function incrementReview() {
-    let count = Number(localStorage.getItem('totalReviews')) || 0;
+    let count = Number(localStorage.getItem('totalReviews')) || 1;
+    updateVisits(count);
     localStorage.setItem('totalReviews', ++count);
 }
 
-window.onload = (event) => {
-    incrementReview();
+function updateVisits(count) {
+    let text = (count > 1) ? `You have visited this page ${count} times.` : 'This is your first review.';
+    visits.textContent = text;
 }
+
+incrementReview();
